3c9c0cd5202af4f83c2eb9a10d1d27d7cea0fe9a
John Foley <jfoley@cs.umass.edu>
2016-10-04 12:06:01 -0400

    compile/run split out

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# need to be connected to the internet to work the very first time
+mvn install # download dependencies if needed & compile
+
--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 CRFSUITE="$HOME/bin/crfsuite-0.12/bin/crfsuite"
-
-mvn install # download dependencies if needed & compile
 java -jar target/starter-0.1-SNAPSHOT.jar --crfsuite=${CRFSUITE} --class=PER --trainingStart=5
